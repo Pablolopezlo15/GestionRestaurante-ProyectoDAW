cf6df281c782e9709e123afa92185f1465f5759a
Pablolopezlo15 <plopezlozano12@gmail.com>
2024-04-26 19:57:14 +0200

    Carta componente

--- src/Components/Carta.js
+++ src/Components/Carta.js
@@ -1,21 +1,50 @@
+import React, { useState } from "react";
+import { useEffect } from "react";
+import { getFirestore, collection, getDocs, setDoc } from "firebase/firestore";
 import { Link } from "react-router-dom";
-
+import '../assets/css/carta.css';
 
 function Carta() {
 
+    const [carta, setCarta] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const db = getFirestore();
+            const data = await getDocs(collection(db, "carta"));
+            setCarta(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+        }
+        fetchData();
+    }, []);
 
     return (
         <>
             <div className="container">
-                <div className="row">
+                <div>
                     <div className="col-md-12">
                         <h1>Carta</h1>
                         <p>En esta sección se mostrará la carta del restaurante</p>
-                        <Link to="/">Volver al Inicio</Link>
-                        <Link to="/login">Iniciar Sesión</Link>
+                        {/* <Link to="/">Volver al Inicio</Link>
+                        <Link to="/login">Iniciar Sesión</Link> */}
                     </div>
                 </div>
-            </div>
+
+                <div className="section">
+                    {
+                        carta.map((item) => {
+                            return (
+                                <div className="card" key={item.nombre}>
+                                    <img src={item.imagen} alt={item.nombre} />
+                                    <h3>{item.nombre}</h3>
+                                    <p><strong>Precio: </strong>{item.precio} €</p>
+                                    <p><strong>Ingredientes: </strong>{item.ingredientes}</p>
+                                </div>
+                            );
+                        })
+                    }
+                </div>
+
+                </div>
         </>
     );
 }
